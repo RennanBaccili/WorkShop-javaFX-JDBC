8702094e3cddeeac436f6cdbd035e5333c70451b
Rennan Bacili <rennanbaccili@gmail.com>
2023-06-30 08:02:10 -0300

    Department (Dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,8 +45,9 @@ public class DepartmentListController implements Initializable {
 	private ObservableList<Department> obsList; //vou carregar eles da obs list
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml",parentStage);
 	}
 	
 	//forma de injetar dependencia
@@ -73,5 +83,24 @@ public class DepartmentListController implements Initializable {
 		// instancia no obsLista os dados da lista de department buscados pelo find all
 		//a partir do obs list podemos instanciar na table do java fx a lista
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	//METODO para carregar tela de dialogo modal
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception","Erro loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
---
+++ src/gui/util/Utils.java
@@ -0,0 +1,12 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils { // para acessar o stage que o controler está, se clicar no botão ele pega o stage do botão
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+	}
+}
+
